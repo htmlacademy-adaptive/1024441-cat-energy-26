ec7ba0e164bfeba8abf7059821942d5c366743a7
Keks <keks@htmlacademy.ru>
2022-08-15 15:37:02 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/burger.js
+++ 10/js/burger.js
@@ -1,5 +1,12 @@
+let header = document.querySelector('.main-header');
+let navigation = document.querySelector('.main-nav');
 let menu = document.querySelector('.main-nav__list');
-let burger = document.querySelector('.main-nav__toggle');
+let burger = document.querySelector('.main-header__toggle');
+let width=document.body.clientWidth;
+
+header.classList.remove('main-header--nojs');
+navigation.classList.remove('main-nav--nojs');
+menu.classList.remove('main-nav__list--nojs');
 
 burger.onclick = function() {
   menu.classList.toggle('main-nav__list--open');
